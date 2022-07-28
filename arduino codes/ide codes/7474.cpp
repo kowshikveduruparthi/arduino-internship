155f0e0de81a151089f71f10a808eb9cae2c9da4
veeracharyulu <96780031+kowshikveduruparthi@users.noreply.github.com>
2022-07-28 20:30:38 +0530

    Update 7474.cpp

--- arduino codes/ide codes/7474.cpp
+++ arduino codes/ide codes/7474.cpp
@@ -1,34 +1,37 @@
 #include <Arduino.h>
-
-int Z=0,Y=1,X=0,W=1;
-int D,C,B,A;
-
-
-void disp_7447(D,C,B,A){
-
-  digitalWrite(2, A); //LSB
-  digitalWrite(3, B);
-  digitalWrite(4, C);
-  digitalWrite(5, D); //MSB
-
+int A,B,C,D;
+int a,b,c,d;
+void disp_7447(int A,int  B, int C,int D){
+digitalWrite(2,A);
+digitalWrite(3,B);
+digitalWrite(4,C);
+digitalWrite(5,D);
+}
+ 
+void setup(){
+pinMode(2,OUTPUT);
+pinMode(3,OUTPUT);
+pinMode(4,OUTPUT);
+pinMode(5,OUTPUT);
+pinMode(6,INPUT);
+pinMode(7,INPUT);
+pinMode(8,INPUT);
+pinMode(9,INPUT);
+pinMode(13,OUTPUT);
 }
 
-void setup() {
-    pinMode(2, OUTPUT);
-    pinMode(3, OUTPUT);
-    pinMode(4, OUTPUT);
-    pinMode(5, OUTPUT);
-    pinMode(6, INPUT);
-    pinMode(7, INPUT);
-    pinMode(8, INPUT);
-    pinMode(9, INPUT);
+void loop(){
+digitalWrite(13,LOW);
+a = digitalRead(6);
+b = digitalRead(7);
+c =digitalRead(8);
+d = digitalRead(9);
+digitalWrite(13,HIGH);
+A = (a&&!b&&!c&&!d)||(!a&&b&&c&&d);
+B = (!a&&!b&&c&&d)||(!a&&b&&!c)||(!a&&b&&!d);
+C = (!a&&!c&&d)||(!a&&c&&!d);;
+D = (!a&&!d)||(!d&&!c&&!b);
+disp_7447(D,C,B,A);
+delay(1000);
 }
 
-void loop() {
-  A=0;
-  B=(W&&!X&&!Y&&!Z) || (!W&&X&&!Y&&!Z) || (W&&!X&&Y&&!Z) || (!W&&X&&Y&&!Z);
-  C=(W&&X&&!Y&&!Z) || (!W&&!X&&Y&&!Z) || (W&&!X&&Y&&!Z) || (!W&&X&&Y&&!Z);
-  D = (W&&X&&Y&&!Z)||(!W&&!X&&!Y&&Z);
-
-  disp_7447(D,C,B,A);
-}
